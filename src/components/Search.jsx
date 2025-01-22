c0bd5b4dfa0a3bcc8de911719f34154fdd53bcae
dipali0210 <nidoni8488@gmail.comgit config --global user.name dipali0210git push -u origin maingit push -u origin maingit config --global user.email nidoni8488@gmail.com>
2025-01-22 14:43:24 +0530

    Done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,41 +1,48 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import NoSearch from './../assets/noFound.webp';
-
+import blankimg from '../assets/blankMovi.webp';
+import axios from 'axios';
 
 const Search = () => {
     const location = useLocation();
     const navigate = useNavigate();
-    const [search, setSearch] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
+    const [search, setSearch] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const Api_key = 'c45a857c193f6302f2b5061c3b85e743';
 
     useEffect(() => {
-        if (location.state?.key) {
+        if (location.state && location.state.key) {
             setLoading(true);
-            console.log("New key from location.state:", location.state.key);
-            setSearch(location.state.key);
-            setLoading(false);
+            axios
+                .request(`https://api.themoviedb.org/3/search/movie?api_key=${Api_key}&language=en-US&query=${location.state.key}`)
+                .then((response) => {
+                    console.log("Search results:", response.data.results);
+                    setSearch(response.data.results);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+                .finally(() => {
+                    setLoading(false);
+                });
         }
-    }, [location.state.key, search])
+    }, [location.state?.key]);
 
     const handledclick = (e) => {
         console.log(e);
         navigate('/movie-detail', { state: { key: e } });
-    }
-
+    };
 
     return (
-        <>{
-            search.length == 0 ?
+        <>
+            {search.length === 0 ? (
                 <div className='d-flex justify-content-center align-items-center vh-100 text-center'>
-                    <img src={NoSearch} alt="" />
+                    <img src={NoSearch} alt="No results found" />
                 </div>
-
-                :
-
+            ) : (
                 <div>
-
                     <div>
                         {loading ? (
                             <div className="loader-container">
@@ -44,27 +51,26 @@ const Search = () => {
                         ) : (
                             <div className="container text-center">
                                 <div className="row row-cols-4">
-                                    {
-                                        search.map((name, index) => {
-                                            return <div className="col" key={name.id} onClick={() => handledclick(name.id)}>
-                                                <div className="movie-card my-5">
-                                                    <img src={`https://image.tmdb.org/t/p/w500${name?.poster_path}` ? `https://image.tmdb.org/t/p/w500${name?.poster_path}` : blankimg} alt={name?.title} />
-                                                    <p className='text-light fs-6 fw-bold'>{name?.title}</p>
-                                                    <p className='text-light'>{name?.vote_average}</p>
-                                                </div>
+                                    {search.map((name) => (
+                                        <div className="col" key={name.id} onClick={() => handledclick(name.id)}>
+                                            <div className="movie-card my-5">
+                                                <img
+                                                    src={name?.poster_path ? `https://image.tmdb.org/t/p/w500${name.poster_path}` : blankimg}
+                                                    alt={name?.title}
+                                                />
+                                                <p className='text-light fs-6 fw-bold'>{name?.title}</p>
+                                                <p className='text-light'>{name?.vote_average}</p>
                                             </div>
-                                        })
-                                    }
-
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
                         )}
                     </div>
-
                 </div>
-        }
+            )}
         </>
-    )
-}
+    );
+};
 
-export default Search
+export default Search;
--- src/components/UpcomingMovies.jsx
+++ src/components/UpcomingMovies.jsx
@@ -1,26 +1,27 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import '../styles/Home.css'
-import blankimg from '../assets/blankMovi.webp'
-import { useNavigate } from "react-router-dom";
+import '../styles/Home.css';
+import axios from 'axios'; // For API Calls
+import React, { useEffect, useState } from 'react';
+import blankimg from '../assets/blankMovi.webp';
+import { useNavigate } from 'react-router-dom';
 
 const UpcomingMovies = () => {
     const navigate = useNavigate();
-    const [allUpcomingMovies, setAllUpcomingMovies] = useState([]);
+    const [allMovies, setAllMovies] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [count, setCount] = React.useState(1)
+    const [page, setPage] = useState(1);
+
+    const Api_key = 'c45a857c193f6302f2b5061c3b85e743';
     
-
-
-    let Api_key = 'c45a857c193f6302f2b5061c3b85e743';
     useEffect(() => {
-
         setLoading(true);
-
-        axios.request(`https://api.themoviedb.org/3/movie/upcoming?api_key=${Api_key}&language=en-US&page=${count}`).then((response) => {
-            console.log(response);
-            setAllUpcomingMovies((allUpcomingMovies) => [...allUpcomingMovies, ...response.data.results]);
-        })
+        
+        // Fetch upcoming movies for the current page
+        axios
+            .request(`https://api.themoviedb.org/3/movie/upcoming?api_key=${Api_key}&language=en-US&page=${page}`)
+            .then((response) => {
+                console.log(response);
+                setAllMovies(response.data.results);
+            })
             .catch((error) => {
                 console.log(error);
                 setLoading(false);
@@ -28,23 +29,20 @@ const UpcomingMovies = () => {
             .finally(() => {
                 setLoading(false);
             });
-    }, [count])
+    }, [page]);
 
-    const handledclick = (e) => {
-        console.log(e);
-        navigate('/movie-detail', { state: { key: e } });
-    }
+    const handleClick = (movieId) => {
+        navigate('/movie-detail', { state: { key: movieId } });
+    };
 
-    const loadMoreData = () =>{
-        console.log(count);
-        setCount((prevCount) => prevCount + 1);
-        console.log(count);
-    }
-
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= 20) {
+            setPage(newPage);
+        }
+    };
 
     return (
         <div>
-
             <div>
                 {loading ? (
                     <div className="loader-container">
@@ -53,29 +51,53 @@ const UpcomingMovies = () => {
                 ) : (
                     <div className="container text-center">
                         <div className="row row-cols-5">
-                            {
-                                allUpcomingMovies.map((name, index) => {
-                                    return <div className="col" key={name.id} onClick={() => handledclick(name.id)}>
-                                        <div className="movie-card my-5">
-                                            <img src={`https://image.tmdb.org/t/p/w500${name?.poster_path}` ? `https://image.tmdb.org/t/p/w500${name?.poster_path}` : blankimg} alt={name.title} />
-                                            <p className='text-light fs-6 fw-bold'>{name.title}</p>
-                                            <p className='text-light'>{name.vote_average}</p>
-                                        </div>
+                            {allMovies.map((movie) => (
+                                <div className="col" key={movie.id} onClick={() => handleClick(movie.id)}>
+                                    <div className="movie-card my-5">
+                                        <img
+                                            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}` || blankimg}
+                                            alt={movie.title}
+                                        />
+                                        <p className="text-light fs-6 fw-bold">{movie.title}</p>
+                                        <p className="text-light">{movie.vote_average}</p>
                                     </div>
-                                })
-                            }
-
+                                </div>
+                            ))}
                         </div>
-                        <div className='d-flex justify-content-center m-5'>
-                            <button className='btn btn-success' onClick={loadMoreData}>Load More</button>
+                        <div className="d-flex justify-content-center m-5">
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination">
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={() => handlePageChange(page - 1)}>
+                                            Previous
+                                        </button>
+                                    </li>
+                                    {[...Array(5)].map((_, index) => {
+                                        const pageNumber = index + 1 + (page - 1);
+                                        return (
+                                            <li key={pageNumber} className="page-item">
+                                                <button
+                                                    className="page-link"
+                                                    onClick={() => handlePageChange(pageNumber)}
+                                                >
+                                                    {pageNumber}
+                                                </button>
+                                            </li>
+                                        );
+                                    })}
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={() => handlePageChange(page + 1)}>
+                                            Next
+                                        </button>
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
-
                     </div>
                 )}
             </div>
-
         </div>
-    )
-}
+    );
+};
 
-export default UpcomingMovies
+export default UpcomingMovies;
